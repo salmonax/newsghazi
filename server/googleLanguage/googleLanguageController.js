46e9985a64519c8c31712c973bfc753d592db2a2
George A. Michel <sarumonakuba@gmail.com>
2016-12-14 19:51:46 -0800

    (feat) Add GoogleNLP API package and controller, convert aylien to middleware (#15)
    * Start sketching out text extraction in newsController
    
    * (fix) Convert aylienController.js to middlware
    
    * (feat) Add googleLanguageController, cloud API package, .gitignore key
    
    Also, put newsController.js back to how it was before
    
    * Return routes.js to normal
    
    * Return newsController.js to normal
    
    * Put routes.js back to normal
    
    * Update popup.js

---
+++ server/googleLanguage/googleLanguageController.js
@@ -0,0 +1,35 @@
+/*
+
+Google Language API (Alpha)
+
+To use, create credentials at the Google dashboard and copy them to ./googleLanguageKey.json. Guide:
+
+http://www.programmableweb.com/news/how-to-start-using-google-cloud-natural-language-api/how-to/2016/09/01#apiu
+
+*/
+
+var Promise = require('bluebird');
+var googleKey = require('./googleLanguageKey.json');
+var language = require('@google-cloud/language');
+var path = require('path');
+
+var languageClient = language({
+  projectId: googleKey.project_id,
+  keyFilename: path.resolve(__dirname, 'googleLanguageKey.json')
+});
+
+var languageClient = Promise.promisifyAll(languageClient);
+
+exports.analyzeSentiment = function (req, res, next) {
+  var article = res.compoundContent['article'];
+  if (article === undefined) {
+    res.status(500).json({ 'error': 'Server attempted analysis on nonexistent content'})
+    return;
+  }
+  // console.log(article);
+  languageClient.detectSentiment(article, { verbose: true }, function(err, sentiment) {
+      res.compoundContent['sentiment'] = sentiment;
+      next();
+  });
+};
+
