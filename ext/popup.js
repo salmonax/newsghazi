3a25775afc07ae168e374a950f8607e291504646
Efe Surekli <e.surekli@gmail.com>
2016-12-15 13:56:53 -0800

    (feat) Implement regex filtered content highlighter  (#20)
    * Implement content scraper and text highlighter in Extension
    
    * Add regex filtering functionality to the content scraper
    
    * Fix bugs in the web highlighter"
    
    * Update gitignore to ignore debug logs

--- ext/popup.js
+++ ext/popup.js
@@ -10,7 +10,7 @@
  */
 
 
-function getCurrentTabUrl(callback) {
+function getCurrentTabUrl(callback) { 
   // Query filter to be passed to chrome.tabs.query - see
   // https://developer.chrome.com/extensions/tabs#method-query
   var queryInfo = {
@@ -39,6 +39,87 @@ function getCurrentTabUrl(callback) {
     callback(url);
   });
 };
+
+
+//scrapper for content
+const contentScrapper = function() {
+  return $('p').toArray().map(p => p.innerText).join(' ');
+};
+
+//assumptions:
+//1 - all the content is in paragraphs
+//2 - paragraph elements have no incomplete sentences (a sentence can not be in two paragrahs)
+
+//interface: string | regex, string, string
+//Ex: first argument could be /\d+/ for testing numbers. 
+
+//parerga: textContent or innerText ? 
+
+const styleStringInContent = function(str, cssProperty, cssValue) {
+  const beginMarker = '~';
+  const endMarker ='`';
+
+  //mark text and inject html tags:
+  const markerFunction = function(tag, str, startIndex) {   
+    tag.textContent = tag.textContent.slice(0,startIndex) +
+    beginMarker + str + endMarker + tag.textContent.slice(startIndex + str.length);
+  };
+
+  //process the content:
+  $('p').toArray().forEach((paragraph, i) => {
+    // if(i === 5) {debugger;}
+    var text = paragraph.textContent;    
+    if(typeof str ==='string') {
+      var startIndex = text.indexOf(str);
+      if(startIndex !== -1) {
+        markerFunction(paragraph, str, startIndex);
+      }
+    } else {
+      var facts = text.split('.').filter(sentence => str.test(sentence));
+      facts.forEach(fact => markerFunction(paragraph, fact, paragraph.textContent.indexOf(fact)));
+    }
+    //inject html to the paragraph:
+    var htmlElementToInjectBegin = '<span class="highlightItem">';
+    var htmlElementToInjectEnd = '</span>';
+    paragraph.innerHTML = paragraph.innerHTML.replace(/~/gi, htmlElementToInjectBegin);
+    paragraph.innerHTML = paragraph.innerHTML.replace(/`/gi, htmlElementToInjectEnd); 
+  });
+  $('.highlightItem').css(cssProperty,cssValue);
+};
+
+//OLD:
+// const styleStringInContent = function(str, cssProperty, cssValue) {
+//   const marker = '^';
+
+//   //mark text and inject html tags:
+//   var markerFunction = function(tag, str, startIndex) {
+//     var htmlElementToInjectBegin = '<span class="highlightItem">';
+//     var htmlElementToInjectEnd = '</span>';
+//     tag.textContent = tag.textContent.slice(0,startIndex) +
+//     marker + str + marker + tag.textContent.slice(startIndex + str.length);
+//     tag.innerHTML = tag.innerHTML.replace('^', htmlElementToInjectBegin);
+//     tag.innerHTML = tag.innerHTML.replace('^', htmlElementToInjectEnd);
+//   };
+
+//   //process the content:
+//   $('p').toArray().forEach((paragraph, i) => {
+//     // if(i === 5) {debugger;}
+//     var text = paragraph.textContent;    
+//     if(typeof str ==='string') {
+//       var startIndex = text.indexOf(str);
+//       if(startIndex !== -1) {
+//         markerFunction(paragraph, str, startIndex);
+//       }
+//     } else {
+//       var facts = text.split('.').filter(sentence => str.test(sentence));
+//       facts.forEach(fact => markerFunction(paragraph, fact, paragraph.textContent.indexOf(fact)));
+//     }
+//   });
+//   $('.highlightItem').css(cssProperty,cssValue);
+// };
+
+
+
 
 
 getCurrentTabUrl(function(tabUrl) {
--- server/trends/googleTrendsCookie.example.js
+++ server/trends/googleTrendsCookie.example.js
@@ -7,11 +7,12 @@ function promiseArr(keywords, timePeriod) {
       uri: 'http://www.google.com/trends/fetchComponent?q=' + keyword + '&cid=TIMESERIES_GRAPH_0&export=3&' + timePeriod,
       headers: {
         //Personal cookie can be found in Chrome, this cookie will not work
-        'Cookie': 'APISID=phZcBBd_oF_clAao/A2biC9KLw1Wk42F4H; HSID=AvCAPi1_gcLNPHp04; NID=90\=ljp9bPedFIvbHjyJu_ASSrzAyOjG8bql-141rlIlMUAFUl2whg2O7g6u8Fa0_I5Nf3WteTzfPF4UEp1K5BZw_FLqRXrpdICasdbkRT24zGGkdJHloBLmNZ55hJb2EgUtfsglxlevfPTIeM5bSZ0SmQejORetaXY_yWUvLxQ5NgsxFjMw8eBuqFYruG3aOMhK4jCrc2p8evYPwLMU5t5z1YqdjvBsJw0pWHTHpM7WWWqHhEVvPgZh8SwvAdCTMidd4Q9TTa3bX77lA8hG_kvcaXGApb1ppelSHCE607viCgNYy4pBHAMYypWW9cgX_WxwOmhzvbSjNn21gJZw6TUkadGhEt8vWYjbJUW7q; SAPISID=cZpNlAneFfMDfB3FLx/AB-QxJFQYeWxEY6zB; SID=7gOUsm_siHAB7zmta3cPjuZM-Nos7Ok4pEekdyB_DPpWH3CspzMf4fxnJ7QoWelTrapcMg.; SSID=ASVarA9Iip_lMFg-w;'
+        'Cookie': 'APISID=39Tg-dAKv2HyraJi/A1kby1JyD2IIwJit2; HSID=ArgzpPTHG1k5nKTtW; NID=91\=lhNl2aBMLJ0Ju-AeBLkIqsI1GH2sV5IHlTcXGaEtIzt7O6E6n_rnBpZwF_iYvhmcdYKFeDcohCImXLSp7lWEkx2TXpwDV32_oiWHHt1D6b187xp1rBa1vn4iJtOX2KHZvzUDjWz_9hCAGVxhi8gGe6JcpxGb8UREnBLESgZMHR27vubxnZ88WzZuXjZmufasGWCk5dGB-uj7URG5NFHW5lFSWUjNzWhtAH7r9cdCKcmknyDX6To; SAPISID=wh8KhGr93ZQ385Lh/ANqm3CMImitRM0sH-; SID=FwTjGh1PyEQyBA8ejFhgcUCCy7KmW9UPI1Kstp6hy1lNfWfpXiAk9DBfn59VFFKsnt-Z5g.; SSID= AH39tUnl0zupUfVME;'
       }
     })
     .then(function (htmlString) {
       return parseJSON(htmlString, arr[index].split(','));
     });
   });
+ // 91=lhNl2aBMLJ0Ju-AeBLkIqsI1GH2sV5IHlTcXGaEtIzt7O6E6n_rnBpZwF_iYvhmcdYKFeDcohCImXLSp7lWEkx2TXpwDV32_oiWHHt1D6b187xp1rBa1vn4iJtOX2KHZvzUDjWz_9hCAGVxhi8gGe6JcpxGb8UREnBLESgZMHR27vubxnZ88WzZuXjZmufasGWCk5dGB-uj7URG5NFHW5lFSWUjNzWhtAH7r9cdCKcmknyDX6To
 }
