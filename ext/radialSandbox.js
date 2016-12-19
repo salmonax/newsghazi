08bf92d973537b16111ae34d3fa99a0b514495d7
George A. Michel <sarumonakuba@gmail.com>
2016-12-18 23:09:18 -0800

    (feat) Add x-axis, robust mouse tracking to sentiment graph

--- ext/radialSandbox.js
+++ ext/radialSandbox.js
@@ -13,8 +13,8 @@ var initJSON = {
   sadness: '0.00' };
 
 var updateJSON = { 
-  anger: '0.90',
-  disgust: '0.84',
+  anger: '0.50',
+  disgust: '0.44',
   fear: '0.23',
   joy: '0.83',
   sadness: '0.35' 
@@ -29,6 +29,7 @@ const EMOTION_COLORS = {
 };
 
 renderRadialGraph(initJSON);
+renderSentimentGraph(dummySentences);
 getBlendedColor(updateJSON);
 
 // This does a bit of bespoke magic and blends the colors together
@@ -125,26 +126,6 @@ function renderRadialGraph(docEmotions) {
     .attr("stroke", "grey")
     .attr("fill","red");
 
-  // BEGIN graph thing for whatsitsface
-
-  var scores = dummySentences.map(sentence => 
-    sentence.sentiment.score*sentence.sentiment.magnitude*50);
-
-  var sentimentFunction = d3.svg.line()
-    .x((d, i) => i*1.5)
-    .y((d, i) => d+80)
-    .interpolate('linear');
-
-  var secondSvg = d3.select('#pentagon').append('svg')
-
-  secondSvg.append('path')
-    .attr('d', sentimentFunction(scores))
-    .attr('stroke', 'black')
-    .attr('fill', 'none');
-
-
-  // END graph thing
-
   svgContainer.selectAll('.emotion-label')
     .data(lineData)
     .enter()
@@ -213,3 +194,83 @@ function getPolyPoints(n, radius, weights) {
   }
   return { perimeter, radials };
 };
+
+function renderSentimentGraph(sentences) {
+  var scores = sentences.map(sentence => 
+    sentence.sentiment.score*sentence.sentiment.magnitude*50);
+
+  var graphWidth = 335;
+  var offsetX = 0;
+  var offsetY = 40;
+
+  var widthFactor = graphWidth/scores.length;
+
+  var sentimentFunction = d3.svg.line()
+    .x((d, i) => i*widthFactor)
+    .y((d, i) => -d+offsetY)
+    .interpolate('linear');
+
+  var secondSvg = d3.select('#pentagon').append('svg')
+    .attr({ width: graphWidth, height: 80 })
+    .attr('class', 'sentiment-svg');
+
+  secondSvg.append('line')
+    .attr({
+      x1: 0,
+      y1: offsetY,
+      x2: graphWidth,
+      y2: offsetY
+    })
+    .attr('stroke', 'black')
+    .attr('stroke-width', 1)
+    .attr('opacity', 0.5)
+    .style('stroke-dasharray',('1,1'));
+
+  var path = secondSvg.append('path')
+    .attr('d', sentimentFunction(scores))
+    .attr('stroke', 'black')
+    .attr('fill', 'none')
+    .attr('pointer-events', 'none');
+
+  pathEl = path.node();
+  pathLength = pathEl.getTotalLength();
+
+  var lastNewIndex = 0;
+  secondSvg.on('mousemove', () => {
+    var x = d3.event.offsetX;
+    var beginning = x, end = pathLength, target;
+    var index = Math.min(Math.round(x/graphWidth*scores.length),scores.length-1);
+    if (index !== lastNewIndex) {
+      lastNewIndex = index;
+      // Put events that only fire on index change here
+      // console.log(sentences[index].text.content);
+      // console.log(x);
+    }
+    while (true) {
+      target = Math.floor((beginning + end) / 2);
+      pos = pathEl.getPointAtLength(target);
+      if ((target === end || target === beginning) && pos.x !== x) {
+          break;
+      }
+      if (pos.x > x)      end = target;
+      else if (pos.x < x) beginning = target;
+      else                break; //position found
+    }
+    d3.select('.emotion-graph-handle')
+      .attr({
+        cx: x,
+        cy: pos.y
+      });
+  });
+
+  secondSvg.append('circle')
+    .attr({
+      cx: 3,
+      cy: offsetY,
+      r: 4,
+      fill: 'blue',
+      // visibility: 'hidden'
+    })
+    .attr('class','emotion-graph-handle')
+    .attr('pointer-events', 'none');
+}
