73029fc59d5ad9d2bb41d5f600308955336f23ce
George A. Michel <sarumonakuba@gmail.com>
2016-12-18 23:19:59 -0800

    (feat) Add x-axis, robust mouse tracking to sentiment graph (#41)

--- ext/sentimentGraph.js
+++ ext/sentimentGraph.js
@@ -19,21 +19,54 @@ function renderSentimentGraph(sentences) {
     .attr({ width: graphWidth, height: 80 })
     .attr('class', 'sentiment-svg');
 
-  secondSvg.append('path')
+  secondSvg.append('line')
+    .attr({
+      x1: 0,
+      y1: offsetY,
+      x2: graphWidth,
+      y2: offsetY
+    })
+    .attr('stroke', 'black')
+    .attr('stroke-width', 1)
+    .attr('opacity', 0.5)
+    .style('stroke-dasharray',('1,1'));
+
+  var path = secondSvg.append('path')
     .attr('d', sentimentFunction(scores))
     .attr('stroke', 'black')
     .attr('fill', 'none')
-    .on('mouseover', () => {
-      var x = d3.event.offsetX;
-      var y = d3.event.offsetY;
-      var index = Math.round(x/graphWidth*scores.length);
-      console.log(sentences[index].text.content);
-      d3.select('.emotion-graph-handle')
-        .attr({
-          cx: x,
-          cy: y
-        });
-    });
+    .attr('pointer-events', 'none');
+
+  pathEl = path.node();
+  pathLength = pathEl.getTotalLength();
+
+  var lastNewIndex = 0;
+  secondSvg.on('mousemove', () => {
+    var x = d3.event.offsetX;
+    var beginning = x, end = pathLength, target;
+    var index = Math.min(Math.round(x/graphWidth*scores.length),scores.length-1);
+    if (index !== lastNewIndex) {
+      lastNewIndex = index;
+      // Put events that only fire on index change here
+      // console.log(sentences[index].text.content);
+      // console.log(x);
+    }
+    while (true) {
+      target = Math.floor((beginning + end) / 2);
+      pos = pathEl.getPointAtLength(target);
+      if ((target === end || target === beginning) && pos.x !== x) {
+          break;
+      }
+      if (pos.x > x)      end = target;
+      else if (pos.x < x) beginning = target;
+      else                break; //position found
+    }
+    d3.select('.emotion-graph-handle')
+      .attr({
+        cx: x,
+        cy: pos.y
+      });
+  });
 
   secondSvg.append('circle')
     .attr({
@@ -43,5 +76,6 @@ function renderSentimentGraph(sentences) {
       fill: 'blue',
       // visibility: 'hidden'
     })
-    .attr('class','emotion-graph-handle');
+    .attr('class','emotion-graph-handle')
+    .attr('pointer-events', 'none');
 }
